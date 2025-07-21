89871354b9699c991c4ffbec68b5153603f7e7cf
josephpedley <josephpedley49@gmail.com>
2025-07-21 16:25:51 +0100

    Altered config to point to support file, added error messages to exclude from failing tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: 'cypress/support/e2e.js', // point to your support file path here
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here if needed
+      return config;
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,24 +1,11 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import './commands'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   if (err.message.includes("Unexpected token '<'")) {
-    // Prevent test from failing when HTML is returned instead of JSON
-    return false
+    return false;
   }
-})
+  if (err.message.includes('POPOVER: Option "title" provided type "undefined"')) {
+    return false;
+  }
+  // Let other errors fail the test
+});
