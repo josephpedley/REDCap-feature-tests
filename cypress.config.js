24e3f05ebe41773f9d80b66c6be760a5fa24556a
josephpedley <josephpedley49@gmail.com>
2025-07-31 11:34:42 +0100

    upgraded reporter to have vids in report

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,29 @@
 const { defineConfig } = require("cypress");
-require('dotenv').config(); // ✅ KEEP THIS
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 
 module.exports = defineConfig({
   projectId: '3pazfm',
 
-  reporter: 'mochawesome',
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
   reportDir: 'cypress/reports',
   overwrite: false,
   html: false,
-  json: true
+  json: true,
+  charts: true,
+  embeddedScreenshots: true,
+  inlineAssets: true,
+  saveAllAttempts: false
 },
  video: true,
   screenshotOnRunFailure: true,
-  
+
   e2e: {
     redirectionLimit: 999,
     supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
-      // ✅ Add custom task
       on('task', {
         getLatestCsv() {
           const downloadsFolder = path.resolve('cypress/downloads');
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,5 @@
 import './commands'
+import 'cypress-mochawesome-reporter/register';
 
 
 Cypress.on('uncaught:exception', (err, runnable) => {
@@ -12,9 +13,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   if (err.message.includes("document.form.submit is not a function")) {
     return false;
   }
-   if (err.message.includes(" cannot call methods on button prior to initialization")) {
-    return false;
-  }
- 
+
+  
   // Let other errors fail the test
 });
